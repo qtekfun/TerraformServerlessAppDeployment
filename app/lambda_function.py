f9902c64a9db31084f2463d48000432cdcdf9086
Qtekfun <qtekfun@gmail.com>
2024-01-26 20:09:03 +0100

    fix: simplify lambda code

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,7 +1,11 @@
 # lambda_function.py
 
+import json
+
 def lambda_handler(event, context):
+
+    # Return the response with a 200 status code
     return {
         'statusCode': 200,
-        'body': 'Hello from my Lambda!'
+        'body': "Hello, I'm fine"
     }
