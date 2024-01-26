0b40b62660d96fd791fedae30ba9a33ab900e61f
Qtekfun <qtekfun@gmail.com>
2024-01-26 21:01:58 +0100

    feat: manage parameter

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -4,6 +4,11 @@ import json
 # import pymysql
 import os
 
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 # Retrieve environment variables
 s3_bucket_name = os.environ['S3_BUCKET_NAME']
 rds_host = os.environ['RDS_HOST']
@@ -13,6 +18,8 @@ rds_db_name = os.environ['RDS_DB_NAME']
 
 def lambda_handler(event, context):
 
+    logging.info(f"Received event: {json.dumps(event)}")
+    
     # connection = pymysql.connect(host=rds_host,
     #                             user=rds_username,
     #                             password=rds_password,
@@ -21,8 +28,20 @@ def lambda_handler(event, context):
     
     # connection.close()
 
+    # Extract the parameter from the incoming JSON request
+    try:
+        request_body = json.loads(event['body'])
+        parameter_value = request_body.get('parameter', 'unknown')
+    except json.JSONDecodeError:
+        parameter_value = 'unknown'
+
+    # Construct the response body including the parameter value
+    response_body = f"Hello, I'm fine. Your env vars are {s3_bucket_name} and {rds_host}.\n"
+    response_body += f"The parameter you sent me was {parameter_value}\n"
+    response_body += f"event is {event}\n"
+
     # Return the response with a 200 status code
     return {
         'statusCode': 200,
-        'body': f"Hello, I'm fine. Your env vars are {s3_bucket_name} and {rds_host}"
+        'body': response_body
     }
