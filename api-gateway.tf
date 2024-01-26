53550d3180191088227f9c714275d2d685441bfe
Qtekfun <qtekfun@gmail.com>
2024-01-26 17:19:56 +0100

    fix: include stage for api-gateway

---
+++ api-gateway.tf
@@ -0,0 +1,35 @@
+resource "aws_apigatewayv2_api" "app_api" {
+  name          = "app-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id            = aws_apigatewayv2_api.app_api.id
+  integration_type  = "AWS_PROXY"
+  integration_uri   = aws_lambda_function.serverless_app.invoke_arn
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.app_api.id
+  route_key = "POST /myResource"
+
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "lambda_stage" {
+  api_id      = aws_apigatewayv2_api.app_api.id
+  name        = "prod"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigateway_invoke_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.serverless_app.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+output "api_gateway_uri" {
+  value = "${aws_apigatewayv2_api.app_api.api_endpoint}/${aws_apigatewayv2_stage.lambda_stage.name}/myResource"
+}
