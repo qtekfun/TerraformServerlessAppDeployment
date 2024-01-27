9bb15d4bd91f0d0c0679af441465e389b1830245
Qtekfun <qtekfun@gmail.com>
2024-01-27 12:27:49 +0100

    feat: include cloudwatch logging

--- lambda.tf
+++ lambda.tf
@@ -58,3 +58,14 @@ resource "aws_lambda_function" "serverless_app" {
     aws_lambda_layer_version.pymysql_layer.arn
   ]
 }
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name = "/aws/lambda/${aws_lambda_function.serverless_app.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role_policy_attachment" "serverless_app_log_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
